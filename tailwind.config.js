066952bf996ae06026a49ea1e856c41709050ec0
Gumbraise <vikiclashonyt@gmail.com>
2022-04-27 11:38:24 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        weather: {
+          'dark': '#000918',
+          'lightblue': '#13aefc',
+          'darkblue': '#1173f3',
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
